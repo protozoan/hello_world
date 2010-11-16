0100082ea55e9346b3d2402e6809cf3ea6dd68be
bkm <buggum@gmail.com>
2010-11-16 11:37:11 -0800

    adding another line to push to master

--- hell.rb
+++ hell.rb
@@ -6,3 +6,4 @@ if I can ever get it to github.com
 So, i am doing this from github. This will be confusing
 
 Adding a line from the hello_dev branch to fetch to origin/master
+Adding another line from the hello_dev branch to fetch to origin/master
