4309b4bfd45becae8c6b4e9041f5c4139be66ec9
bkm <buggum@gmail.com>
2010-11-16 07:21:19 -0800

    added line to fetch to origin

--- hell.rb
+++ hell.rb
@@ -3,8 +3,6 @@ Sample text
 to alter later
 if I can ever get it to github.com
 
-<<<<<<< HEAD
-This line is for merging into main
-=======
 So, i am doing this from github. This will be confusing
->>>>>>> origin/master
+
+Adding a line from the hello_dev branch to fetch to origin/master
